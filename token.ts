2fab7aa5f435656b14ae3305391f527929e70045
fujidig <fuji.rosen@gmail.com>
2017-02-12 07:52:02 +0900

    add token.ts

---
+++ token.ts
@@ -0,0 +1,98 @@
+﻿class Token {
+}
+
+class TokenInt extends Token {
+    type(): string { return "int"; }
+}
+
+class TokenConstant extends Token {
+    val: number;
+    constructor(val: number) {
+        super();
+        this.val = val;
+    }
+    type(): string { return "constant"; }
+}
+
+class TokenIdentifier extends Token {
+    name: string;
+    constructor(name: string) {
+        super();
+        this.name = name;
+    }
+    type(): string { return "identifier"; }
+}
+
+class TokenIf extends Token {
+    type(): string { return "if"; }
+}
+
+class TokenElse extends Token {
+    type(): string { return "else"; }
+}
+
+class TokenWhile extends Token {
+    type(): string { return "while"; }
+}
+
+class TokenReturn extends Token {
+    type(): string { return "return"; }
+}
+
+class TokenRparen extends Token {
+    type(): string { return "("; }
+}
+class TokenLparen extends Token {
+    type(): string { return ")"; }
+}
+class TokenComma extends Token {
+    type(): string { return ","; }
+}
+class TokenPlus extends Token {
+    type(): string { return "+"; }
+}
+class TokenMinus extends Token {
+    type(): string { return "-"; }
+}
+class TokenAsterisk extends Token {
+    type(): string { return "*"; }
+}
+class TokenSlash extends Token {
+    type(): string { return "/"; }
+}
+class TokenPercent extends Token {
+    type(): string { return "%"; }
+}
+class TokenEqual extends Token {
+    type(): string { return "="; }
+}
+class TokenSemicolon extends Token {
+    type(): string { return ";"; }
+}
+class TokenColon extends Token {
+    type(): string { return ":"; }
+}
+class TokenRbrace extends Token {
+    type(): string { return "{"; }
+}
+class TokenLbrace extends Token {
+    type(): string { return "}"; }
+}
+class TokenLt extends Token {
+    type(): string { return "<"; }
+}
+class TokenGt extends Token {
+    type(): string { return ">"; }
+}
+class TokenLe extends Token {
+    type(): string { return "<="; }
+}
+class TokenGe extends Token {
+    type(): string { return ">="; }
+}
+class TokenEq extends Token {
+    type(): string { return "=="; }
+}
+class TokenNe extends Token {
+    type(): string { return "!="; }
+}
