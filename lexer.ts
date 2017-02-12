aa561099cfcf68cae4d893137f1d6756851c9e01
fujidig <fuji.rosen@gmail.com>
2017-02-12 10:13:02 +0900

    add lexer.ts

---
+++ lexer.ts
@@ -0,0 +1,61 @@
+﻿const RESERVED_WORD = {
+    "int": TokenInt,
+    "if": TokenIf,
+    "else": TokenElse,
+    "while": TokenWhile,
+    "return": TokenReturn,
+}
+
+const SYMBOLS = {
+    "<=": TokenLe,
+    ">=": TokenGe,
+    "==": TokenEq,
+    "!=": TokenNe,
+    "(": TokenRparen,
+    ")": TokenLparen,
+    ",": TokenComma,
+    "+": TokenPlus,
+    "-": TokenMinus,
+    "*": TokenAsterisk,
+    "/": TokenSlash,
+    "%": TokenPercent,
+    "=": TokenEqual,
+    ";": TokenSemicolon,
+    ":": TokenColon,
+    "{": TokenRbrace,
+    "}": TokenLbrace,
+    "<": TokenLt,
+    ">": TokenGt,
+};
+
+class Lexer {
+    src: string;
+    constructor(src: string) {
+        this.src = src;
+    }
+
+    lex(): Array<Token> {
+        var tokens : Array<Token> = [];
+        var src = this.src;
+        while (src != "") {
+            var m: RegExpMatchArray;
+            if (m = src.match(/^\s+/)) {
+            } else if (m = src.match(/^[a-zA-Z_][a-zA-Z_0-9]*/)) {
+                if (RESERVED_WORD.hasOwnProperty(m[0])) {
+                    tokens.push(new RESERVED_WORD[m[0]]);
+                } else {
+                    tokens.push(new TokenIdentifier(m[0]));
+                }
+            } else if (m = src.match(/^[0-9]+/)) {
+                tokens.push(new TokenConstant(Number(m[0])));
+            } else if (m = src.match(/<=|>=|==|!=|\(|\)|,|\+|-|\*|\/|%|=|;|:|{|}|<|>/)) {
+                tokens.push(new SYMBOLS[m[0]]);
+            }
+            src = src.slice(m[0].length);
+        }
+        return tokens;
+    }
+}
+
+
+console.log(new Lexer("int main() { while (1) { print(42); } }").lex());
